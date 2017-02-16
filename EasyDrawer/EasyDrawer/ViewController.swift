ee86b33d02fef315ea8afe44f798535a8b409a6e
王小帅 <505040542@qq.com>
2017-02-16 16:21:05 +0800

    no message

--- EasyDrawer/EasyDrawer/ViewController.swift
+++ EasyDrawer/EasyDrawer/ViewController.swift
@@ -13,11 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        let someone = Demo()
-        someone.name = "xiaoming"
-        someone.eat()
+    
     }
 
     override func didReceiveMemoryWarning() {
